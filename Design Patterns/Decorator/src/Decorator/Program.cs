f9abb3bc14cc7805fdb094cb466e7c5abb2bd5b7
Kalkwst <kalafatiskwstas@gmail.com>
2021-09-08 11:07:48 +0300

    Feature/decorator (#3)
    * Add Decorator implementation
    
    * Add README.md for Decorator pattern
    
    * Add Decorator Patter Structure Diagram
    
    * Update README.md
    
    Co-authored-by: kkalafatis <kkalafatis@novibet.com>

---
+++ Design Patterns/Decorator/src/Decorator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Decorator.Decorators;
+
+namespace Decorator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string record = "Hello World!";
+
+			DataSourceDecorator processed = new CompressionDecorator(
+												new EncryptionDecorator(
+													new FileDataSource("out/DecoratorDump.txt"), "8UHjPgXZzXCGkhxV2QCnooyJexUzvJrO"));
+
+			processed.WriteData(record);
+
+			IDataSource plain = new FileDataSource("out/DecoratorDump.txt");
+
+			Console.WriteLine("--------------------------------------");
+			Console.WriteLine($"Original Data: {record}");
+			Console.WriteLine($"Processed:  { processed.ReadData()}");
+			Console.WriteLine($"Plain: {plain.ReadData()}");
+		}
+	}
+}
+			
